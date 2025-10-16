99caa5c65388c2e1cc9b641c330f2e37bb8ba7de
eduardo <eduardomendes418@gmail.com>
2025-10-16 13:14:58 -0300

    [CRD-12] feat - create types

---
+++ src/types/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+export interface UserFormData {
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
